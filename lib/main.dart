33b3acb7e4c7bc145db8fb63c502f181c039f4d8
sseeaann10 <seanivan.ridulme@gmail.com>
2025-03-03 22:59:18 +0100

    Firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:projet_parcour_france/firebase_options.dart';
 import 'screens/home_screen.dart'; // Importe l'écran d'accueil
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
